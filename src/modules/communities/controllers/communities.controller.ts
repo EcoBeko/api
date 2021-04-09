514d9d9d438519d253b318f0dcd67ebdea547b6a
RayDarar <dfqgth400@gmail.com>
2021-04-09 06:41:58 +0600

    work(community.module): finished

--- src/modules/communities/controllers/communities.controller.ts
+++ src/modules/communities/controllers/communities.controller.ts
@@ -1,5 +1,16 @@
 import { Protected } from "@/core/decorators/protected.decorator";
-import { Controller, Get, Inject, Query } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Inject,
+  Param,
+  Post,
+  Put,
+  Query,
+} from "@nestjs/common";
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { CommunitiesService } from "../services/communities.service";
 
@@ -23,5 +34,85 @@ export class CommunitiesController {
     @Query("description") description: string,
   ) {
     return this.service.getCommunities(title, description);
+  }
+
+  @Post("/")
+  @ApiOperation({
+    summary: "Create a community",
+  })
+  @ApiResponse({
+    status: 201,
+    description: "Created",
+  })
+  public async create(
+    @Body("userId") userId: string,
+    @Body("title") title: string,
+    @Body("description") description: string,
+    @Body("imagePath") imagePath: string,
+  ) {
+    return this.service.create(userId, title, description, imagePath);
+  }
+
+  @Get("/:id")
+  @ApiOperation({
+    summary: "Get community by id",
+  })
+  @ApiResponse({
+    status: 200,
+    description: "Ok",
+  })
+  @ApiResponse({
+    status: 404,
+    description: "Not found",
+  })
+  public async getCommunity(
+    @Query("userId") userId: string,
+    @Param("id") id: string,
+  ) {
+    return this.service.getCommunity(userId, id);
+  }
+
+  @Get("/:id/subs")
+  @ApiOperation({
+    summary: "Get community subs",
+  })
+  @ApiResponse({
+    status: 200,
+    description: "Ok",
+  })
+  public async getCommunitySubs(@Param("id") id: string) {
+    return this.service.getSubs(id);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Put("/:id/subs/:user_id")
+  @ApiOperation({
+    summary: "Update sub status",
+  })
+  @ApiResponse({
+    status: 200,
+    description: "Ok",
+  })
+  public async updateStatus(
+    @Param("id") id: string,
+    @Param("user_id") userId: string,
+    @Body("status") status: string,
+  ) {
+    return this.service.updateStatus(id, userId, status);
+  }
+
+  @Post("/:id/subs/:user_id")
+  @ApiOperation({
+    summary: "Subscribe user to a community",
+  })
+  @ApiResponse({
+    status: 201,
+    description: "Subscribed",
+  })
+  public async subscribe(
+    @Param("id") id: string,
+    @Param("user_id") userId: string,
+  ) {
+    return this.service.subscribe(id, userId);
   }
 }
--- src/modules/communities/services/communities.service.ts
+++ src/modules/communities/services/communities.service.ts
@@ -1,5 +1,5 @@
 import { PGPoolService } from "@/core/pool/services/pg-pool.service";
-import { Inject, Injectable } from "@nestjs/common";
+import { Inject, Injectable, NotFoundException } from "@nestjs/common";
 
 @Injectable()
 export class CommunitiesService {
@@ -7,9 +7,52 @@ export class CommunitiesService {
   private readonly db: PGPoolService;
 
   public async getCommunities(title: string, description: string) {
-    return this.db.query(
-      "select * from f_search_communities($1,$2)",
-      [title, description],
+    return this.db.query("select * from f_search_communities($1,$2)", [
+      title,
+      description,
+    ]);
+  }
+
+  public async getCommunity(userId: string, id: string) {
+    const [
+      community,
+    ] = await this.db.query("select * from f_get_community($1,$2)", [
+      userId,
+      id,
+    ]);
+
+    if (!community) throw new NotFoundException("Community not found");
+
+    return community;
+  }
+
+  public async create(
+    userId: string,
+    title: string,
+    description: string,
+    imagePath: string,
+  ) {
+    const [result] = await this.db.query(
+      "call p_create_community($1,$2,$3,$4)",
+      [title, imagePath, description, userId],
     );
+
+    return result;
+  }
+
+  public async getSubs(id: string) {
+    return this.db.query("select * from f_get_subs($1)", [id]);
+  }
+
+  public async updateStatus(id: string, userId: string, status: string) {
+    return this.db.query("call p_update_sub_status($1,$2,$3)", [
+      userId,
+      id,
+      status,
+    ]);
+  }
+
+  public async subscribe(id: string, userId: string) {
+    return this.db.query("call p_subscribe_user($1,$2)", [userId, id]);
   }
 }
